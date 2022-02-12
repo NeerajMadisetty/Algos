6f301df8ce731189934aae20b6dcf525eca59f68
NeerajMadisetty <neerajkumar.m068@gmail.com>
2022-02-11 18:26:22 -0700

    Create preorder_iterative.java

---
+++ Trees/preorder_iterative.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+        if(root == null) return Collections.EMPTY_LIST;
+        
+        List<Integer> result = new ArrayList<>();
+        
+        Stack<TreeNode> s = new Stack<>();
+        s.push(root);
+        
+        while(s.size() > 0){
+            TreeNode curr = s.pop();
+            result.add(curr.val);
+            if(curr.right!= null) s.push(curr.right);
+            if(curr.left!= null) s.push(curr.left);
+        }
+        
+        return result;
+        
+    }
+}
